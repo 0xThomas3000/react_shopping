64849cbe5dbe16cdee70fe859b772312d9c5581f
tuanit168 <tuanit168@gmail.com>
2022-04-17 09:50:20 +0700

    Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the Navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const Shop = () => {
   return <h1>I am the Shop Page</h1>;
@@ -8,8 +19,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
+      {" "}
       {/* Everything inside of this Component can be "routable" */}
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
