1a7b6e6a1899865195fe44f3a2c1b675708b48e7
tuanit168 <tuanit168@gmail.com>
2022-04-17 09:30:55 +0700

    Router Outlet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 
+const Shop = () => {
+  return <h1>I am the Shop Page</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      {" "}
       {/* Everything inside of this Component can be "routable" */}
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
